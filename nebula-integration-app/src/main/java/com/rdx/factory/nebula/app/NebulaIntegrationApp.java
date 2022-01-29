191c9ab19bdc7d83beead33f204859baba5c6937
shubhamkokul <shubhamkokul@gmail.com>
2022-01-29 16:48:05 -0700

    First-Commit

---
+++ nebula-integration-app/src/main/java/com/rdx/factory/nebula/app/NebulaIntegrationApp.java
@@ -0,0 +1,10 @@
+package com.rdx.factory.nebula.app;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NebulaIntegrationApp {
+    public static void main(String[] args) {
+        SpringApplication.run(NebulaIntegrationApp.class, args);
+    }
+}
